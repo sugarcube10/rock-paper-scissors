51f9dc74cd3893a92a58f0015e4f2aa311038dae
sugarcube10 <217878983+sugarcube10@users.noreply.github.com>
2026-02-12 07:16:52 +0530

    Add function playRound()

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+let humanScore = 0, computerScore = 0;
+
 function getComputerChoice() {
 
     let computerChoice = Math.floor(Math.random() * 3);
@@ -9,6 +12,32 @@ function getHumanChoice() {
 
     let playerChoice = prompt("enter rock, paper or scissors");
     return playerChoice;
+
+}
+
+function playRound(computerChoice, humanChoice) {
+    
+    humanChoice = humanChoice.toLowerCase();
+
+    if (computerChoice === 0) 
+        computerChoice = "scissors";
+    else if (computerChoice === 1)
+        computerChoice = "rock";
+    else   
+        computerChoice = "paper";
+
+    if (humanChoice === computerChoice)
+        console.log("its a draw");
+    else if ((humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "scissors" && computerChoice === "paper"))
+    {
+        console.log("You win!! " + humanChoice + " beats " + computerChoice);
+        humanScore++;
+    }
+    else
+        {
+            console.log("You lose, " + computerChoice + " beats " + humanChoice)
+            computerScore++;
+        }
 
 }
 
